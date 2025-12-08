55e6d147c5a36e0b8a2969b74d576d922d781bc8
hangyudu <hangyudu@tencent.com>
2025-12-08 16:14:26 +0800

    [ch4][update]refactor from elf.

--- os/src/mm/memory_set.rs
+++ os/src/mm/memory_set.rs
@@ -270,6 +270,7 @@ impl MemorySet {
         )
     }
 
+    /// map_high map application address high
     pub fn map_high(memory_set: &mut MemorySet) {
         // map trampoline
         memory_set.map_trampoline();
@@ -286,6 +287,7 @@ impl MemorySet {
         );
     }
 
+    /// map_low map application address low
     pub fn map_low(elf : &xmas_elf::ElfFile,memory_set: &mut MemorySet) -> usize {
         // map program headers of elf, with U flag
         let elf = xmas_elf::ElfFile::new(elf_data).unwrap();
