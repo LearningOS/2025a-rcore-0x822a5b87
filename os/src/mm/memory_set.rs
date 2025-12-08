64aa558d3ec5a1b6c541f6d8a73d518aa2dd78a1
hangyudu <hangyudu@tencent.com>
2025-12-08 16:53:39 +0800

    [ch5][update]add VM and PM

--- os/src/mm/memory_set.rs
+++ os/src/mm/memory_set.rs
@@ -153,6 +153,9 @@ impl MemorySet {
     /// 1. Map a `MapArea` to the page table.
     /// 2. If any input files are provided, copy the data from the input files into the newly initialized `MapArea`.
     /// 3. Push MapArea into `MemorySet`
+    /// 1. Map a `MapArea` to the page table.
+    /// 2. If any input files are provided, copy the data from the input files into the newly initialized `MapArea`.
+    /// 3. Push MapArea into `MemorySet`
     fn push(&mut self, mut map_area: MapArea, data: Option<&[u8]>) {
         map_area.map(&mut self.page_table);
         // copy app data from files(such as an elf executable) if any input files is provided.
@@ -179,6 +182,9 @@ impl MemorySet {
             "[new_kernel].text [{:#x}, {:#x})",
             stext as usize, etext as usize
         );
+        info!("[new_kernel].text [{:#x}, {:#x})", stext as usize, etext as usize);
+        info!("[new_kernel].rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+        info!("[new_kernel].data [{:#x}, {:#x})", sdata as usize, edata as usize);
         info!(
             "[new_kernel].rodata [{:#x}, {:#x})",
             srodata as usize, erodata as usize
@@ -188,6 +194,7 @@ impl MemorySet {
             sdata as usize, edata as usize
         );
         info!(
+            "[new_kernel].bss [{:#x}, {:#x})",
             "[new_kernel].bss [{:#x}, {:#x})",
             sbss_with_stack as usize, ebss as usize
         );
@@ -259,10 +266,13 @@ impl MemorySet {
     /// Include sections in elf and trampoline and TrapContext and user stack,
     /// also returns user_sp_base and entry point.
     pub fn from_elf(elf_data: &[u8]) -> (Self, usize, usize) {
-        let elf = xmas_elf::ElfFile::new(elf_data).unwrap();
         let mut memory_set = Self::new_bare();
-        Self::map_high(&mut memory_set);
-        let user_stack_top = Self::map_low(&elf, &mut memory_set);
+        // map program headers of elf, with U flag
+        let elf = xmas_elf::ElfFile::new(elf_data).unwrap();
+
+        Self::map_higher_addr(&mut memory_set);
+        let user_stack_top = Self::map_lower_addr(&mut memory_set, &elf);
+
         (
             memory_set,
             user_stack_top,
@@ -270,11 +280,9 @@ impl MemorySet {
         )
     }
 
-    /// map_high map application address high
-    pub fn map_high(memory_set: &mut MemorySet) {
+    fn map_higher_addr(memory_set: &mut MemorySet) {
         // map trampoline
         memory_set.map_trampoline();
-
         // map TrapContext
         memory_set.push(
             MapArea::new(
@@ -350,6 +358,10 @@ impl MemorySet {
                 );
             }
         }
+    fn map_lower_addr(memory_set: &mut MemorySet, elf: & xmas_elf::ElfFile) -> usize {
+        // map .text/.rodata/... from elf
+        let max_end_vpn = Self::map_elf(memory_set, &elf);
+
         // map user stack with U flags
         let max_end_va: VirtAddr = max_end_vpn.into();
         let mut user_stack_bottom: usize = max_end_va.into();
@@ -382,6 +394,7 @@ impl MemorySet {
     /// Create a new address space by copy code&data from an exited process's address space.
 
     /// Create a new address space by copy code&data from a exited process's address space.
+    /// Create a new address space by copy code&data from an exited process's address space.
     pub fn from_existed_user(user_space: &Self) -> Self {
         let mut memory_set = Self::new_bare();
         // map trampoline
@@ -403,6 +416,44 @@ impl MemorySet {
     }
 
     fn map_elf(memory_set: &mut MemorySet, elf: &xmas_elf::ElfFile) -> VirtPageNum {
+        let elf_header = elf.header;
+        let magic = elf_header.pt1.magic;
+        assert_eq!(magic, [0x7f, 0x45, 0x4c, 0x46], "invalid elf!");
+        let ph_count = elf_header.pt2.ph_count();
+        let mut max_end_vpn = VirtPageNum(0);
+        for i in 0..ph_count {
+            let ph = elf.program_header(i).unwrap();
+            if ph.get_type().unwrap() == xmas_elf::program::Type::Load {
+                // Notice that the elf file has been loaded into virtual memory
+                let start_va: VirtAddr = (ph.virtual_addr() as usize).into();
+                let end_va: VirtAddr = ((ph.virtual_addr() + ph.mem_size()) as usize).into();
+                let mut map_perm = MapPermission::U;
+                let ph_flags = ph.flags();
+                if ph_flags.is_read() {
+                    map_perm |= MapPermission::R;
+                }
+                if ph_flags.is_write() {
+                    map_perm |= MapPermission::W;
+                }
+                if ph_flags.is_execute() {
+                    map_perm |= MapPermission::X;
+                }
+                let map_area = MapArea::new(start_va, end_va, MapType::Framed, map_perm);
+                max_end_vpn = map_area.vpn_range.get_end();
+                // init MapArea and copy program header into the newly created MapArea
+                trace!("[user]: start loading elf");
+                memory_set.push(
+                    map_area,
+                    Some(&elf.input[ph.offset() as usize..(ph.offset() + ph.file_size()) as usize]),
+                );
+            }
+        }
+
+        max_end_vpn
+    }
+
+
+    fn map_elf(memory_set: &mut MemorySet, elf: & xmas_elf::ElfFile) -> VirtPageNum {
         let elf_header = elf.header;
         let magic = elf_header.pt1.magic;
         assert_eq!(magic, [0x7f, 0x45, 0x4c, 0x46], "invalid elf!");
